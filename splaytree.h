0987af61d808c8e8756c2489a90827698d7a7429
culot <culot@FreeBSD.org>
2016-06-01 21:35:33 +0200

    Add skeleton for the splay tree implementation

---
+++ splaytree.h
@@ -0,0 +1,27 @@
+#pragma once
+
+/*
+ * Splay tree - a self-Adjusting binary search tree,
+ * as first described by Daniel Dominic Sleator and Robert Endre Tarjan.
+ *
+ * https://www.cs.cmu.edu/~sleator/papers/self-adjusting.pdf
+ */
+templace<class T>
+class SplayTree {
+  private:
+    struct Node {
+      Node*  left;
+      Node*  right;
+      Node*  parent;
+      T      data;
+
+      void   rotateLeft()   {}
+      void   rotateRight()  {}
+      void   splay()        {}
+    };
+
+  public:
+    void   insert(const T& data) {}
+    void   remove(const T& data) {}
+    Node*  find(const T& data)   {}
+};
