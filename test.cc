40feb64847d99d6b0bcadb1a40c0f9a75c88d874
culot <culot@FreeBSD.org>
2016-06-16 21:42:31 +0200

    Basic test

--- test.cc
+++ test.cc
@@ -1,9 +1,22 @@
 #include <cassert>
+#include <iostream>
 
 #include "splaytree.h"
 
 int main(void) {
-  //Splaytree<int> tree;
+  Splaytree<int> tree;
+
+  assert(tree.size() == 0);
+
+  tree.insert(100);
+  assert(tree.size() == 1);
+
+  Splaytree<int>::Iterator it = tree.begin();
+  assert(*it == 100);
+
+  tree.insert(5).insert(71).insert(12028).insert(13);
+
+  assert(tree.size() == 5);
 
   return 0;
 }
