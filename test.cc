555cf422352f0a768e736616c1c4ca28f3fe5b6d
culot <culot@FreeBSD.org>
2016-06-17 13:05:37 +0200

    Improve test verbosity

--- test.cc
+++ test.cc
@@ -6,13 +6,19 @@
 int main(void) {
   Splaytree<int> tree;
 
+  std::cout << "Check that tree is initially empty... ";
   assert(tree.size() == 0);
+  std::cout << "OK" << std::endl;
 
+  std::cout << "Insert a node and check that tree contains exactly 1 element... ";
   tree.insert(100);
   assert(tree.size() == 1);
+  std::cout << "OK" << std::endl;
 
+  std::cout << "Check that tree contains the previously inserted value... ";
   Splaytree<int>::Iterator it = tree.begin();
   assert(*it == 100);
+  std::cout << "OK" << std::endl;
 
   tree.insert(5).insert(71).insert(12028).insert(13);
 
